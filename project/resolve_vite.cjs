1fc17b00fb6677fab68df9c6bcd63c0b2fd75b8d
ioriimasu <sayedimam.fahim@gmail.com>
2025-09-09 08:04:22 +0600

    chore(cleanup D): move orphan public assets to _trash; remove unused Next-only component and debug helpers

--- project/resolve_vite.cjs
+++
@@ -1,1 +0,0 @@
-console.log('cwd', process.cwd()); console.log('paths', JSON.stringify(module.paths, null, 2)); console.log('vite', require.resolve('vite'));
--- project/resolve_vite.js
+++
@@ -1,1 +0,0 @@
-console.log('cwd', process.cwd()); console.log('module.filename', module.filename); console.log('paths', JSON.stringify(module.paths, null, 2)); console.log('vite', require.resolve('vite'));
